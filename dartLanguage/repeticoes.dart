f49f65553fdf3e210fbfc59721b7a53ebdab7c21
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2020-08-30 13:30:46 -0300

    Aula Realizando Repetições (Loops) finalizada

---
+++ dartLanguage/repeticoes.dart
@@ -0,0 +1,27 @@
+void main() {
+  for (int i = 0; i <= 10; i+=2) {
+    print(i);
+  }
+
+  print("\n");
+
+  for (int i = 100; i >= 10; i-=5) {
+    print(i);
+  }
+
+  print("\n");
+
+  int j = 0;
+  while (j <= 10) {
+    print(j);
+    j++;
+  }
+
+  print("\n");
+
+  int k = 0;
+  do {
+    print(k);
+    k++;
+  } while (k < 10);
+}
