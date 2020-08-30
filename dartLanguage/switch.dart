426e625ff396a9ccf2f2bee72ddbe33b96279831
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2020-08-30 12:53:41 -0300

    Aula Decisões LImitadas (Switch) finalizada

---
+++ dartLanguage/switch.dart
@@ -0,0 +1,17 @@
+void main() {
+  String linguagem = "Dart";
+
+  switch (linguagem) {
+    case "Dart":
+      print("Dart!!");
+      break;
+    case "Java":
+      print("Java :(");
+      break;
+    case "Swift":
+      print("Swift :(");
+      break;
+    default: 
+      print("Outra...");
+  }
+}
