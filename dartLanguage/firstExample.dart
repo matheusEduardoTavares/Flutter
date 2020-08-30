020a5bbace9130612de41d5d8f980deb162bf53e
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2020-08-29 22:10:37 -0300

    Aula Conheça a Linguagem Dart finalizada

---
+++ dartLanguage/firstExample.dart
@@ -0,0 +1,17 @@
+/* 
+Dart é CASE SENSITIVE
+Dart PRECISA de ;
+Dart é tipado, e usa OO
+*/
+void main() {
+  String name = 'nome';
+  print('Your name is ' + name);
+  print(teste(5, 2));
+
+  print('Hello');
+  print('Name');
+}
+
+int teste(int a, int b) {
+  return a + b;
+}
