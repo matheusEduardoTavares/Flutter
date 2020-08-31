20dd4a6ce140f80c4e70714c41d9d17d0ab804c3
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2020-08-31 14:21:29 -0300

    Aula 39 - Coluna e Textos - finalizada

--- contador_de_pessoas/lib/main.dart
+++ contador_de_pessoas/lib/main.dart
@@ -4,6 +4,20 @@ void main(){
   // runApp(new MaterialApp());
   runApp(MaterialApp(
       title: "Contador de Pessoas",
-      home: Container(color: Colors.white)
+      // home: Container(color: Colors.white)
+      home: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Pessoas: 0",
+                style: TextStyle(color: Colors.white,
+                    fontWeight: FontWeight.bold)),
+            Text("Pode Entrar!",
+                style: TextStyle(color: Colors.white,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 30.0
+                )
+            )
+          ]
+      )
   ));
 }
