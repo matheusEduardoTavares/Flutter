b6bb263e0d2a6501c2ad481fa8edab347b5be1bb
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2020-09-02 11:32:17 -0300

    Aula 46 - TextField e Decoration - finalizada

--- calculadora_imc/lib/main.dart
+++ calculadora_imc/lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Home()
-  ));
+  runApp(MaterialApp(home: Home()));
 }
 
 class Home extends StatefulWidget {
@@ -15,17 +13,32 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Calculadora de IMC'),
-        centerTitle: true,
-        backgroundColor: Colors.green,
-        actions: <Widget> [
-          IconButton(
-            icon: Icon(Icons.refresh),
-            onPressed: () {}
-          )
-        ]
-      )
-    );
+        appBar: AppBar(
+            title: Text('Calculadora de IMC'),
+            centerTitle: true,
+            backgroundColor: Colors.green,
+            actions: <Widget>[
+              IconButton(icon: Icon(Icons.refresh), onPressed: () {})
+            ]),
+        backgroundColor: Colors.white,
+        body: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Icon(Icons.person_outline, size: 120.0, color: Colors.green),
+              TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: "Peso (Kg)",
+                      labelStyle: TextStyle(color: Colors.green)),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.green, fontSize: 25.0)),
+              TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: "Altura (cm)",
+                      labelStyle: TextStyle(color: Colors.green)),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.green, fontSize: 25.0))
+            ]));
   }
 }
