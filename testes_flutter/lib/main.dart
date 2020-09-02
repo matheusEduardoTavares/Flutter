c539763099e96493dc8e4b1f97241fc11d59c2fd
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2020-09-02 00:02:18 -0300

    Testes finalizados

--- testes_flutter/lib/main.dart
+++ testes_flutter/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main(){
-
+  runApp(MaterialApp(
+    title: '',
+    home: Home()
+  ));
 }
 
 class Home extends StatefulWidget {
@@ -10,12 +13,63 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _pessoas = 0;
+  String _text = "Pode Entrar!";
+
+  void _increment(int val) {
+    setState(() {
+      _pessoas += val;
+
+      if (_pessoas < 0){
+        _text = "Mundo alternativo";
+      }
+      else if (_pessoas > 10) {
+        _text = "Lotado!";
+      }
+      else {
+        _text = "Pode Entrar!";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Stack(
-      Image.asset(
-        'image/restaurant.png'
-      )
-    )
+        children: <Widget> [
+          Image.asset(
+              'images/restaurant.jpg',
+              fit: BoxFit.cover,
+              height: 1000
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Pessoas: $_pessoas', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget> [
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: FlatButton(
+                      child: Text("+ 1", style: TextStyle(fontSize: 40, color: Colors.white)),
+                      onPressed: () => _increment(1)
+                    )
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: FlatButton(
+                      child: Text("-1", style: TextStyle(fontSize: 40, color: Colors.white)),
+                      onPressed: () {
+                        _increment(-1);
+                      }
+                    )
+                  )
+                ]
+              ),
+              Text("$_text", style: TextStyle(fontSize: 30, color: Colors.white, fontStyle: FontStyle.italic))
+            ],
+          )
+        ]
+    );
   }
 }
