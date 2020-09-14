f65564e04e7f3cdf156cda965801ca95e984e56d
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2020-09-14 00:59:57 -0300

    Pequenas melhorias realizadas no App - Poder digitar números reais e limpar todos os campos caso algum fique vazio - feito

--- conversor/lib/main.dart
+++ conversor/lib/main.dart
@@ -58,19 +58,37 @@ class _HomeState extends State<Home>{
   double dolar;
   double euro;
 
+  void _clearAll(){
+    realController.text = "";
+    dolarController.text = "";
+    euroController.text = "";
+  }
+
   void _realChanged(String text) {
+    if (text.isEmpty){
+      _clearAll();
+      return;
+    }
     double real = double.parse(text);
     dolarController.text = (real/dolar).toStringAsFixed(2);
     euroController.text = (real/euro).toStringAsFixed(2);
   }
 
   void _dolarChanged(String text) {
+    if (text.isEmpty){
+      _clearAll();
+      return;
+    }
     double dolar = double.parse(text);
     realController.text = (dolar * this.dolar).toStringAsFixed(2);
     euroController.text = (dolar * this.dolar / euro).toStringAsFixed(2);
   }
 
   void _euroChanged(String text) {
+    if (text.isEmpty){
+      _clearAll();
+      return;
+    }
     double euro = double.parse(text);
     realController.text = (euro * this.euro).toStringAsFixed(2);
     dolarController.text = (euro * this.euro / dolar).toStringAsFixed(2);
@@ -174,6 +192,7 @@ Widget buildTextField(String label, String prefix, TextEditingController c, Func
       color: Colors.amber, fontSize: 25.0
     ),
     onChanged: f,
-    keyboardType: TextInputType.number
+    // keyboardType: TextInputType.number
+    keyboardType: TextInputType.numberWithOptions(decimal: true),
   );
 }
