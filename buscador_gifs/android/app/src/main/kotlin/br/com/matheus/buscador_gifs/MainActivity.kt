b26c4c3a4cf0dfcb96aedcc2f79b631c1645eab7
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2020-10-02 14:17:47 -0300

    Aula 81 - Requisitando os Dados - finalizada

---
+++ buscador_gifs/android/app/src/main/kotlin/br/com/matheus/buscador_gifs/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.matheus.buscador_gifs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ buscador_gifs/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:buscador_gifs/ui/home_page.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'buscador_gifs',
+      home: HomePage()
+    )
+  );
+}
