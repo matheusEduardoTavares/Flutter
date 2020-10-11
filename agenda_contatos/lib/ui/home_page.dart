3f773e5da815e80f1f6b08a287d7f25f633a9b30
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2020-10-11 14:58:47 -0300

    Aula 100 - Cartões dos Contatos - finalizada

--- agenda_contatos/lib/ui/home_page.dart
+++ agenda_contatos/lib/ui/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:agenda_contatos/helpers/contact_helper.dart';
+import 'dart:io';
 
 class HomePage extends StatefulWidget {
   @override 
@@ -75,9 +76,61 @@ class _HomePageState extends State<HomePage> {
         padding: EdgeInsets.all(10),
         itemCount: contacts.length,
         itemBuilder: (context, index) {
-          
+          return _contactCard(context, index);
         }
       )
+    );
+  }
+
+  Widget _contactCard(BuildContext context, int index) {
+    //Cada contato irá estar dentro de um Card, que é 
+    //um widget interessante para separar vários elementos
+    //iguais. Mas esse Widget não tem ações nele como 
+    //onPressed e etc, por isso colocaremos ele dentro
+    //de um GestureDetector
+    return GestureDetector(
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Row(
+            children: <Widget>[
+              //Para fazer uma imagem redonda, colocaremos
+              //a imagem dentro de um container e o 
+              //container faremos ficar redondo.
+              Container(
+                width: 80.0,
+                height: 80.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: contacts[index].img != null ? 
+                      FileImage(File(contacts[index].img)) :
+                        AssetImage("images/person.png")
+                  )
+                )
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(contacts[index].name ?? "", 
+                      style: TextStyle(fontSize: 22.0, 
+                        fontWeight: FontWeight.bold)
+                    ),
+                    Text(contacts[index].email ?? "", 
+                      style: TextStyle(fontSize: 18.0)
+                    ),
+                    Text(contacts[index].phone ?? "", 
+                      style: TextStyle(fontSize: 18.0)
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        )
+      ),
     );
   }
 }
