af16f5defa7d94a8d1dc5742b6ee04038df41c27
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2020-10-17 22:16:29 -0300

    Aula 102 - Layout da Tela de Contato - finalizada

--- agenda_contatos/lib/main.dart
+++ agenda_contatos/lib/main.dart
@@ -1,10 +1,11 @@
+import 'package:agenda_contatos/ui/contact_page.dart';
 import 'package:flutter/material.dart';
 import 'package:agenda_contatos/ui/home_page.dart';
 
 void main(){
   runApp(MaterialApp(
     title: 'Agenda de contatos',
-    home: HomePage(),
+    home: ContactPage(),
     debugShowCheckedModeBanner: false
   ));
 }
